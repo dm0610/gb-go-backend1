cd8a951389abf573816dc1a8fde24e4c3ab2fb4c
Стрельников Дмитрий Валерьевич <dmvstrelnikov@beeline.ru>
2022-05-10 16:48:18 +0300

    upd

--- hw04/filesrv/main.go
+++ hw04/filesrv/main.go
@@ -5,49 +5,60 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
+	"path/filepath"
 )
 
-type UploadHandler struct {
+type FileHandler struct {
 	HostAddr  string
 	UploadDir string
 }
 
-func (h *UploadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		http.Error(w, "Unable to read file", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-	data, err := ioutil.ReadAll(file)
-	if err != nil {
-		http.Error(w, "Unable to read file", http.StatusBadRequest)
-		return
-	}
-	filePath := h.UploadDir + "/" + header.Filename
-	err = ioutil.WriteFile(filePath, data, 0777)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Unable to save file", http.StatusInternalServerError)
-		return
-	}
-	fileLink := h.HostAddr + "/" + header.Filename
-	fmt.Fprintln(w, fileLink)
+func main() {
+	uploadHandler := &FileHandler{UploadDir: "./upload", HostAddr: "http://localhost:8080"}
+	http.HandleFunc("/upload", uploadHandler.uploadFileHandler())
+	http.Handle("/files/", http.StripPrefix("/files", http.FileServer(http.Dir(uploadHandler.UploadDir))))
+	http.HandleFunc("/list", uploadHandler.ListFilesHandler())
+
+	log.Print("Server started on localhost:8080, use /upload for uploading files and /files/{fileName} for downloading")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func main() {
+func (h *FileHandler) uploadFileHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		file, header, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, "Unable to read file", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			http.Error(w, "Unable to read file", http.StatusBadRequest)
+			return
+		}
+		filePath := h.UploadDir + "/" + header.Filename
+		err = ioutil.WriteFile(filePath, data, 0777)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Unable to save file", http.StatusInternalServerError)
+			return
+		}
+		fileLink := h.HostAddr + "/" + header.Filename
+		fmt.Fprintln(w, fileLink)
+		fmt.Fprintf(w, "File %s has been successfully uploaded", header.Filename)
+	})
+}
 
-	uploadHandler := &UploadHandler{
-		UploadDir: "upload",
-	}
-	http.Handle("/upload", uploadHandler)
-	dirToServe := http.Dir(uploadHandler.UploadDir)
-	fs := &http.Server{
-		Addr:         ":8080",
-		Handler:      http.FileServer(dirToServe),
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-	fs.ListenAndServe()
+func (h *FileHandler) ListFilesHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		files, err := ioutil.ReadDir(h.UploadDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, f := range files {
+			//fmt.Fprintln(w, "Name: ", f.Name(), " Size: ", f.Size(), "isDir: ", f.IsDir(), "Extension: ", filepath.Ext(string(f.Name())))
+			fmt.Fprintf(w, "Name: %15v Size: %8v isDir: %8v Extension: %8v \n", f.Name(), f.Size(), f.IsDir(), filepath.Ext(string(f.Name())))
+
+		}
+	})
 }
